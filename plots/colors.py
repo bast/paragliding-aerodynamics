86d7cd5ae484f28139a25c123a5c605da471c0d5
Radovan Bast <bast@users.noreply.github.com>
2023-06-24 15:37:34 +0200

    cvd-friendly colors

---
+++ plots/colors.py
@@ -0,0 +1,11 @@
+# https://jfly.uni-koeln.de/color/
+colors = {
+    #   "black": "rgb(0, 0, 0)",
+    "reddishpurple": "rgb(204, 121, 167)",
+    "orange": "rgb(230, 159, 0)",
+    "bluishgreen": "rgb(0, 158, 115)",
+    #   "yellow": "rgb(240, 228, 66)",
+    "skyblue": "rgb(86, 180, 233)",
+    "vermillion": "rgb(213, 94, 0)",
+    "blue": "rgb(0, 114, 178)",
+}
