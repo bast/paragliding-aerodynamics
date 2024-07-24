065e58f0e2dabbf32e347cdffcfdebd8d46f62d1
Radovan Bast <bast@users.noreply.github.com>
2024-07-24 08:55:15 +0200

    update script

--- plots/run.sh
+++ plots/run.sh
@@ -7,7 +7,7 @@
 set -euo pipefail
 
 if [ ! -f venv.sif ]; then
-  singularity pull https://github.com/bast/singularity-venv/releases/download/0.3.0/venv.sif
+    singularity pull https://github.com/bast/singularity-venv/releases/download/0.4.0/venv.sif
 fi
 
 for file in wing-comparison.py important-points.py moving-polar.py; do
